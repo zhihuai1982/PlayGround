8ac5192e7dd3c9524d2487ab152f4245c6973000
zhihuai1982 <zhihuai1982@zhihuai1982deiMac.local>
2020-06-12 20:48:11 +0800

    sync

--- m6aexp/correlation.R
+++ m6aexp/correlation.R
@@ -1,7 +1,9 @@
 library("Hmisc")
 library("corrplot")
 fer <- read.csv("./TilePlot.csv", header = TRUE)
-fer.cor <- cor(fer)
+fer.cor <- cor(fer,
+  method = c("spearman")
+)
 fer.rcorr <- rcorr(as.matrix(fer))
 
 ##  different color series
@@ -9,4 +11,3 @@ col5 <- colorRampPalette(c("#053061", "#2166AC", "#4393C3", "#92C5DE", "#D1E5F0"
 corrplot(fer.cor,
   col = col5(200),
 )
-
